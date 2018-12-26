afd4f92f7acd7d5528044f8782257d3a6783e60c
Cecilia Tu <cecilia.tu@icloud.com>
2018-12-25 20:08:55 -0800

    revisions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,44 @@
-
+// Initialize variables
 var win = 0;
 var loss = 0;
 var attempts = 10;
-var usedArray = [];
+var usedLetterArray = [];
 var randomLetter = randomLetter;
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-
+// Generate random number between 0-1 * length of alphabet
 randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(randomLetter);
 
 function randomGuess() {
     randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(randomLetter);
-
 }
 
+
+// Take in user input
 document.onkeyup = function (event) {
     var guess = event.key;
 
-    if (guess === randomLetter) {
+    if (guess === randomLetter){
         win++;
-        attempts = 10;
-        usedArray = [];
-    }
-
-    randomGuess();
-    if (guess !== randomLetter) {
         attempts--;
-
+    }    
+    else{
+        loss++;
+        attempts--;
     }
 
-    if (attempts == 0) {
-        loss++;
-        usedArray = [];
+    if (attempts === 0){
+        wins++;
+        usedLetterArray = [];
         attempts = 10;
     }
 
 
-    if (usedArray.indexOf(guess) >= 0) {
+    document.getElementById("win").innerHTML = win;
+    document.getElementById("loss").innerHTML = loss;
+    document.getElementById("attempts").innerHTML = attempts;
 
-    } else {
-
-        usedArray.push(guess);
-        document.getElementById('guess').innerHTML = usedArray;
-        console.log(usedArray);
-
-    }
-
-    document.getElementById('win').innerHTML = win;
-    document.getElementById('loss').innerHTML = loss;
-    document.getElementById('attempts').innerHTML = attempts;
-
+    usedLetterArray.push(guess);
+    document.getElementById("guess").innerHTML = usedLetterArray;
 }
